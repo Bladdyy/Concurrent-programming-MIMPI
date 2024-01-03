c58b99ac632653f05a88f7975db6ab31cc75c52a
Juliusz Pawlus <juliuszpawlus@gmail.com>
2024-01-03 13:55:41 +0100

    Commenting and renumerating descriptors

--- mimpi.c
+++ mimpi.c
@@ -14,45 +14,45 @@
 
 void MIMPI_Init(bool enable_deadlock_detection) {
     channels_init();
-    char val[4];
     char* initalized = getenv("ENTERED");
+    // Sprawdza, czy proces wchodził już do bloku MIMPI.
     if (initalized != NULL && atoi(initalized) == 0) {
+        char val[4];
         sprintf(val, "%d", 1);
-        setenv("ENTERED", val, 1);
-    }
-    else{
-        sprintf(val, "%d", -1);
-        setenv("ENTERED", val, 1);
+        setenv("ENTERED", val, 1);  // Udziela pozowolenia na wejście.
     }
 }
 
 void MIMPI_Finalize() {
     char* initalized = getenv("ENTERED");
+    // Sprawdza, czy proces jest w bloku MIMPI.
     if (initalized != NULL && atoi(initalized) == 1) {
         int rank = MIMPI_World_rank();
+        // Wysyła do pozostałych procesów wiadomość, że zakończył swój blok MIMPI.
+        int *data = malloc(sizeof(int) * MIMPI_World_size());  // Tablica procesów w bloku MIMPI.
+        ASSERT_SYS_OK(chrecv(20, data, sizeof(int) * MIMPI_World_size()));
+        data[rank] = 0;  // Zaznacza w tablicy, że zakończył swój blok MIMPI.
+        ASSERT_SYS_OK(chsend(21, data, sizeof(int) * MIMPI_World_size()));
         for (int i = 0; i < MIMPI_World_size(); i++){
             if (i != rank) {
                 MIMPI_Send(NULL, 0, i, -1);
             }
         }
-        int *data = malloc(sizeof(int) * MIMPI_World_size());
-        int from = 2 * MIMPI_World_size() * (MIMPI_World_size() - 1) + 20;
-        ASSERT_SYS_OK(chrecv(from, data, sizeof(int) * MIMPI_World_size()));
-        data[rank] = 0;
-        ASSERT_SYS_OK(chsend(from + 1, data, sizeof(int) * MIMPI_World_size()));
         char val[4];
         sprintf(val, "%d", -1);
-        setenv("ENTERED", val, -1);
+        setenv("ENTERED", val, 1);  // Odbiera dostęp do funkcji MIMPI.
+
         channels_finalize();
     }
 }
 
 int MIMPI_World_size() {
     char* initalized = getenv("ENTERED");
+    // Sprawdza, czy proces jest w bloku MIMPI.
     if (initalized != NULL && atoi(initalized) == 1) {
         char *size = getenv("WORLD_SIZE");
         if (size != NULL) {
-            return atoi(size);
+            return atoi(size);  // Zwraca ile procesów zostało utworzonych.
         } else {
             return -3;
         }
@@ -64,8 +64,9 @@ int MIMPI_World_size() {
 
 int MIMPI_World_rank() {
     char* initalized = getenv("ENTERED");
+    // Sprawdza, czy proces jest w bloku MIMPI.
     if (initalized != NULL && atoi(initalized) == 1) {
-        char* rank = getenv("MY_RANK");
+        char* rank = getenv("MY_RANK");  // Zwraca wartość rank procesu sprawdzającego.
         if (rank != NULL){
             return atoi(rank);
         }
@@ -85,32 +86,32 @@ MIMPI_Retcode MIMPI_Send(
     int tag
 ) {
     char *initalized = getenv("ENTERED");
-    if (destination == MIMPI_World_rank()) {
+    if (destination == MIMPI_World_rank()) {  // Sprawdza, czy proces nie próbuje wysłać wiadomości do siebie.
         return 1;
-    } else if (destination >= MIMPI_World_size()) {
+    } else if (destination >= MIMPI_World_size()) {  // Sprawdza, czy proces nie próbuje wysłać wiadomości do nieistniejącego procesu.
         return 2;
-    } else if (initalized != NULL && atoi(initalized) == 1) {
-        int dir = MIMPI_World_rank() * (MIMPI_World_size() - 1) * 2 + 21 + destination * 2;
+    } else if (initalized != NULL && atoi(initalized) == 1) {  // Sprawdza, czy proces jest w bloku MIMPI.
+        // Numer deskryptora, do którego zapisywana będzie wiadomość.
+        int dir = 23 + MIMPI_World_size() * 4 + destination * 2 + MIMPI_World_rank() * (MIMPI_World_size() - 1) * 2;
+        // Jeśli rank odbiorcy jest większy od rank nadawcy.
         if (destination > MIMPI_World_rank()) {
             dir -= 2;
         }
-        if (tag == -1){
+        if (tag == -1){  // Wiadomość wysyłana dotyczy zakończenia bloku MIMPI.
             int x = -1;
             ASSERT_SYS_OK(chsend(dir, &x, sizeof(int)));
             return 0;
         }
         else{
-            int *in = malloc(sizeof(int) * MIMPI_World_size());
-            int from = 2 * MIMPI_World_size() * (MIMPI_World_size() - 1) + 20;
-            ASSERT_SYS_OK(chrecv(from, in, sizeof(int) * MIMPI_World_size()));
-            int val = in[destination];
-            ASSERT_SYS_OK(chsend(from + 1, in, sizeof(int) * MIMPI_World_size()));
-            if (val == 0) {
+            int *in = malloc(sizeof(int) * MIMPI_World_size());  // Tablica procesów w bloku MIMPI.
+            ASSERT_SYS_OK(chrecv(20, in, sizeof(int) * MIMPI_World_size()));
+            ASSERT_SYS_OK(chsend(21, in, sizeof(int) * MIMPI_World_size()));
+            if (in[destination] == 0) {  // Jeśli procesu już nie ma w bloku MIMPI.
                 return 3;
             }
-            void* msg = malloc(504);
-            memcpy(msg, &tag, sizeof(int));
-            memcpy(msg + sizeof(int), data, sizeof(*data));
+            void* msg = malloc(512);  // Wiadomość wysyłana.
+            memcpy(msg, &tag, sizeof(int));  // Dodawanie tagu wiadomości.
+            memcpy(msg + sizeof(int), data, sizeof(*data)); // Dodawanie treści wiadomości.
             ASSERT_SYS_OK(chsend(dir, msg, count + sizeof(int)));
             free(msg);
             free(in);
@@ -129,23 +130,24 @@ MIMPI_Retcode MIMPI_Recv(
     int tag
 ) {
     char* initalized = getenv("ENTERED");
-    if (source == MIMPI_World_rank()){
+    if (source == MIMPI_World_rank()){  // Sprawdza, czy proces nie próbuje odczytać wiadomości do siebie.
         return 1;
     }
-    else if (source >= MIMPI_World_size()){
+    else if (source >= MIMPI_World_size()){  // Sprawdza, czy proces nie próbuje odczytać wiadomości do nieistniejącego procesu.
         return 2;
     }
-    else if (initalized != NULL && atoi(initalized) == 1) {
-        int dir = source * (MIMPI_World_size() - 1) * 2 + 20 + MIMPI_World_rank() * 2;
+    else if (initalized != NULL && atoi(initalized) == 1) {  // Sprawdza, czy proces jest w bloku MIMPI.
+        // Numer deskryptora, z którego będzie czytana wiadomość.
+        int dir = 22 + MIMPI_World_size() * 4 + MIMPI_World_rank() * 2 + source * (MIMPI_World_size() - 1) * 2;
+        // Jeśli rank odbiorcy jest większy od rank nadawcy.
         if (source < MIMPI_World_rank()) {
             dir -= 2;
         }
-        int newtag;
+        int newtag;  // Tag odczytywanej wiadomości.
         void* buff = malloc(sizeof(int));
         ASSERT_SYS_OK(chrecv(dir, buff, sizeof(int)));
-
         memcpy(&newtag, buff, sizeof(int));
-        if (newtag == -1){
+        if (newtag == -1){  // Jeśli tag wiadomości oznacza wyjście z bloku MIMPI.
             return 3;
         }
         ASSERT_SYS_OK(chrecv(dir, data, count));
--- mimpirun.c
+++ mimpirun.c
@@ -5,31 +5,33 @@
 #include "mimpi_common.h"
 #include "channel.h"
 
-#include <string.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <pthread.h>
 #include <stdio.h>
 
 int main(int argc, char** argv) {
     if (argc < 3){  // Program dostał mniej, niż 2 argumenty.
         return -1;
     }
-    int n = atoi(argv[1]);  // Ilość programów do odpalenia.
+    int n = atoi(argv[1]);  // Ilość procesów do odpalenia.
+
+    // Zmienna środowiskowa z ilością procesów odpalonych.
     ASSERT_ZERO(setenv("WORLD_SIZE",argv[1], 0));
-    int desc[20];
+
+    int desc[20];  // Deskyptory, które należy zamknąć po otwarciu potrzebnych.
     int pipefd[2];
     int index = 0;
-    pipefd[0] = 0;
-    pipefd[1] = 1;
-    while (pipefd[1] < 19){
+
+    while (pipefd[1] < 19){  // Otwieranie niedozwolonych deskryptorów.
         ASSERT_ZERO(channel(pipefd));
         desc[2 * index] = pipefd[0];
         desc[(2 * index) + 1] = pipefd[1];
         index++;
     }
     index = (index) * 2;
+
+    // Zamykanie nadliczbowo otwartych dobrych.
     if (pipefd[1] > 19){
         ASSERT_ZERO(close(pipefd[1]));
         index--;
@@ -38,40 +40,52 @@ int main(int argc, char** argv) {
         ASSERT_ZERO(close(pipefd[0]));
         index--;
     }
-    for (int i = 0; i < n * (n - 1) + 1; i++){
+
+    // Otwieranie dobrych deskryptorów do komunikacji między składowymi MIMPI oraz procesami.
+    for (int i = 0; i < n * (n + 1) + 1; i++) {
         ASSERT_ZERO(channel(pipefd));
-        printf("%d %d\n", pipefd[0], pipefd[1]);
     }
+
+    // Zamykanie poprzednio otwartych złych deskryptorów.
     for (int i = 0; i < index; i++){
         ASSERT_ZERO(close(desc[i]));
     }
+
+    // Tablica procesów, które wyszły z MIMPI.
     int* in = malloc(sizeof(int) * n);
     for (int i = 0; i < n; i++){
         in[i] = 1;
     }
-    printf("wpisuje %d\n", 2 * n * (n - 1) + 20 + 1);
-    ASSERT_SYS_OK(chsend(2 * n * (n - 1) + 20 + 1, in, sizeof(int) * n));
+    // Umieszcza tablicę procesów w pierwszym pipie.
+    ASSERT_SYS_OK(chsend(21, in, sizeof(int) * n));
+
+    // Inicjalizacja nowych procesów.
     for (int i = 0; i < n; i++){
         pid_t pid = fork();
         ASSERT_SYS_OK(pid);
         if (!pid){
             char val[64];
             sprintf(val, "%d", i);
+            // Zmienna środowiskowa informująca o rank procesu.
             ASSERT_ZERO(setenv("MY_RANK",val, 0));
             sprintf(val, "%d", 0);
+            // Zmienna środowiskowa informująca o tym, czy proces jest w bloku MIMPI.
             ASSERT_ZERO(setenv("ENTERED",val, 0));
-            char* args[argc - 1];
+            char* args[argc - 1]; // Argumenty procesów odpalanych.
             for (int x = 0; x < argc - 2; x++){
                 args[x] = argv[x + 2];
             }
             args[argc - 2] = NULL;
-            ASSERT_SYS_OK(execv(argv[2], args));
+            ASSERT_SYS_OK(execv(argv[2], args)); // Odpalanie nowego procesu.
         }
     }
+
+    // Oczekiwanie na zakończenie wszystkich procesów potomnych.
     for (int i = 0; i < n; i++) {
         ASSERT_SYS_OK(wait(NULL));
     }
-    for (int i = 0; i < 2 * n * (n - 1) + 2; i++){
+    // Zamykanie otwartych deskryptorów.
+    for (int i = 0; i < 2 * (n * (n + 1) + 1); i++){
         ASSERT_ZERO(close(i + 20));
     }
 }
